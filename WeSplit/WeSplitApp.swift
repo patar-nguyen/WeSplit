6df5f8913c11a7b7bbda9f8a5e36e72a889e19ab
Patrick Nguyen <patar.nguyen@gmail.com>
2023-08-07 18:26:37 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Patrick Nguyen on 2023-08-07.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
